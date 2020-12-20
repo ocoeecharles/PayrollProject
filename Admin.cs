255420289be0a50073c532a090e9f61cb5f261ea
Charles Owens <charlesowens@ups.com>
2020-12-20 17:44:37 -0600

    Admin

---
+++ Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Payroll
+{
+    class Admin : Staff
+    {
+        private const float overtimeRate = 45.5f;
+        private const float adminHourlyRate = 30f;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name) : base(name, adminHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            if (HoursWorked > 160)
+                Overtime = overtimeRate * (HoursWorked - 160);
+            TotalPay = BasicPay + Overtime;
+        }
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff + "\nadminHourlyRate = " + adminHourlyRate + "\nHoursWorked = " + HoursWorked + "\nBasicPay = " + BasicPay + "\nOvertime = " + Overtime + "\n\nTotalPay = " + TotalPay;
+        }
+    }
+}
