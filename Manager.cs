11bcc486c73f2afea65033f5128f5386dabf0c3d
Charles Owens <charlesowens@ups.com>
2020-12-20 17:46:51 -0600

    Manager

---
+++ Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Payroll
+{
+    class Manager : Staff
+    {
+        private const float managerHourlyRate = 50;
+        public int Bonus { get; private set; }
+        public Manager(string name) : base(name, managerHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Bonus = 1000;
+            if (HoursWorked > 160)
+                TotalPay = BasicPay + Bonus;
+        }
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff + "\nmanagerHourlyRate = " + managerHourlyRate + "\nHoursWorked = " + HoursWorked + "\nBasicPay = " + BasicPay + "\nBonus = " + Bonus + "\n\nTotalPay = " + TotalPay;
+        }
+    }
+}
